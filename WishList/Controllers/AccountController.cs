e27bf98cc600fe8085b38afd65b034fa35a99149
Aisling McGettigan <aislingdeveloper@hotmail.com>
2021-11-08 14:39:04 +0000

    add login/logout functionality

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -43,18 +43,56 @@ namespace WishList.Controllers
             };
 
             var createUser = _userManager.CreateAsync(appUser).Result;
-                if (!createUser.Succeeded)
+            if (!createUser.Succeeded)
+            {
+                foreach (var error in createUser.Errors)
                 {
-                    foreach (var error in createUser.Errors)
-                    {
-                        ModelState.AddModelError("Password", error.Description);
-                    }
-                    return View("Register", viewModel);
+                    ModelState.AddModelError("Password", error.Description);
                 }
+                return View("Register", viewModel);
+            }
 
             return RedirectToAction("Home/Index");
         }
-    }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Login", viewModel);
+            }
+
+            var signIn = _signInManager.PasswordSignInAsync(viewModel.Email, viewModel.Password, false,false).Result;
+
+            if (!signIn.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+
+            return RedirectToAction("Item/Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Logout()
+        {
+
+            _signInManager.SignOutAsync();
+
+            return RedirectToAction("Home/Index");
+        }
+}
 }
 
 
